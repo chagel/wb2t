0ee76d32a6da5f833396c48e6e393335ad88edbb
Mike Chen <chagel@gmail.com>
2011-08-24 23:28:36 +0800

    Fix timestamp comparison from different time zone

--- wb2t.rb
+++ wb2t.rb
@@ -6,6 +6,7 @@ require 'rubygems'
 require 'twitter'
 require 'simple-rss'
 require 'open-uri'
+require 'time'
 
 Twitter.configure do |config|
   config.consumer_key = 'YOUR DATA'
@@ -15,9 +16,9 @@ Twitter.configure do |config|
 end
 
 class Weibo2Twitter
-  @@weibo_rss = "http://medcl.net/sinarss/?uname=chagel&originalimage=false&titlelimit=200"
-  @@sync_time = "wb2t_st"
-  @@sync_log = "wb2t.log"
+  @@weibo_rss = "http://medcl.net/sinarss/?uname=chagel"
+  @@sync_time = "/Users/mike/dev/workspace/wb2t/wb2t_st"
+  @@sync_log = "/Users/mike/dev/workspace/wb2t/wb2t.log"
 
   def sync
     last_sync_time = load_data
@@ -25,7 +26,7 @@ class Weibo2Twitter
       debug "Reading rss file: #{@@weibo_rss}"
       rss = SimpleRSS.parse(open(@@weibo_rss))
       rss.items.reverse.each_with_index do |item, index|
-        if item.pubDate > last_sync_time
+        if Time.parse(item.pubDate.to_s) > Time.parse(last_sync_time.to_s)
           debug "sync message - #{item.pubDate} - #{item.title}"
           Twitter.update "#{item.title}"
           dump_data item.pubDate
@@ -34,6 +35,7 @@ class Weibo2Twitter
       end
     rescue Exception => e
       debug e.message
+      debug e.backtrace.join "\n"
     end
   end
 
